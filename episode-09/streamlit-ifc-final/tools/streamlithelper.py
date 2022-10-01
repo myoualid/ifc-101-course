74e17dd4f6a826458cbb62780ec1fc16c7356f13
bosonprojets <bosonprojets@gmail.com>
2022-10-01 21:26:13 +0100

    clean up unecessary files

--- episode-09/streamlit-ifc-final/tools/streamlithelper.py
+++
@@ -1,6 +0,0 @@
-def get_project_name(session):
-    return session.ifc_file.by_type("IfcProject")[0].Name
-
-def change_project_name(session):
-    if session.project_name_input:
-        session.ifc_file.by_type("IfcProject")[0].Name = session.project_name_input
